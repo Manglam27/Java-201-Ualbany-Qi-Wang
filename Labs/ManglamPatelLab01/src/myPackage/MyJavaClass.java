622f5b8b182225d9c6d2556f210b152c4fc5851c
Manglam Patel <patelmanglam27@gmail.com>
2024-05-04 01:59:07 -0400

    Just added the first lab

---
+++ Labs/ManglamPatelLab01/src/myPackage/MyJavaClass.java
@@ -0,0 +1,15 @@
+package myPackage;
+/**
+* My first hello world project.
+* @auther Manglam Mukeshkumar Patel
+* @version 1.0
+*/
+public class MyJavaClass {
+	/**
+	 *  Main Java class, that displays Hello world.
+	 */
+	public static void main(String[] arg) {
+		//Display "Hello world"
+		System.out.println("Hello World");
+	}
+}
